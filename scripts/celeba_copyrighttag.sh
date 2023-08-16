ea235a696e8df1d3d24354921432bba6d3ac029d
sidney <sb.sidney.bender@gmail.com>
2023-08-16 17:54:39 +0200

    started asyrp integration

--- scripts/celeba_copyrighttag.sh
+++ scripts/celeba_copyrighttag.sh
@@ -8,7 +8,7 @@ apptainer run --nv python_container.sif python3 train_model.py \
 --model_name "Smiling_confounding_CopyrightTag_celeba_classifier_poisened" \
 --data.confounder_probability 1.0 \
 --data.num_samples 7000
-apptainer run --nv python_container.sif python train_diffusion_model.py \
+apptainer run --nv python_container.sif python3 train_diffusion_model.py \
 --generator_config "peal/configs/generators/ace_generator_CopyrightTag_celeba.yaml"
 apptainer run --nv python_container.sif python3 run_cfkd.py \
 --adaptor_config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba_cfkd.yaml"
