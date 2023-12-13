b5992cd66fda135d2b61e908beab30aac15c8f45
Fahad Ahmed <fahad.ahmed95@live.com>
2023-12-13 17:49:22 +0100

    old teacher code

--- peal/teachers/model2model_teacher.py
+++ peal/teachers/model2model_teacher.py
@@ -7,7 +7,7 @@ class Model2ModelTeacher(TeacherInterface):
         self.dataset = dataset
         self.device = "cuda" if next(self.model.parameters()).is_cuda else "cpu"
 
-    def get_feedback(self, x_counterfactual_list, y_source_list, y_target_list, x_list, base_dir, **kwargs):
+    def get_feedback(self, x_counterfactual_list, y_source_list, y_target_list, x_list, base_dir, y_list, **kwargs):
         feedback = []
         teacher_original = []
         teacher_counterfactual = []
@@ -16,16 +16,16 @@ class Model2ModelTeacher(TeacherInterface):
         for idx, counterfactual in enumerate(x_counterfactual_list):
             pred_original = self.model(x_list[idx].unsqueeze(0).to(self.device)).squeeze(0).detach().cpu().argmax(-1)
             pred_counterfactual = self.model(counterfactual.unsqueeze(0).to(self.device)).squeeze(0).detach().cpu().argmax(-1)
-            pred = self.model(counterfactual.unsqueeze(0).to(self.device)).squeeze(0)
 
             # TODO here has to be somehing added for OOD e.g. with FID score
-            if pred[y_target_list[idx]] > pred[y_source_list[idx]]:
-                feedback.append("true")
-
-            else:
-                feedback.append("false")
-            # TODO here has to be somehing added for OOD e.g. with FID score
-            """if pred_original == y_source_list[idx]:
+            #if y_source_list[idx] == y_list[idx]:                
+            #    if pred_original != pred_counterfactual:
+            #        feedback.append("true")
+            #    else:
+            #        feedback.append("false")
+            #else:
+            #    feedback.append("ood")
+            if pred_original == y_source_list[idx]:
                 if pred_counterfactual == y_target_list[idx]:
                     feedback.append("true")
 
@@ -37,23 +37,23 @@ class Model2ModelTeacher(TeacherInterface):
                     feedback.append("false")
 
                 else:
-                    feedback.append("true")"""
+                    feedback.append("true")
 
             teacher_original.append(pred_original)
             teacher_counterfactual.append(pred_counterfactual)
 
         #import pdb; pdb.set_trace()
-        self.dataset.generate_contrastive_collage(
-            y_counterfactual_teacher_list=teacher_counterfactual,
-            y_original_teacher_list=teacher_original,
-            feedback_list=feedback,
-            x_counterfactual_list=x_counterfactual_list,
-            y_source_list=y_source_list,
-            y_target_list=y_target_list,
-            x_list=x_list,
-            base_path=base_dir,
-            **kwargs,
-        )
+        #self.dataset.generate_contrastive_collage(
+        #    y_counterfactual_teacher_list=teacher_counterfactual,
+        #    y_original_teacher_list=teacher_original,
+        #    feedback_list=feedback,
+        #    x_counterfactual_list=x_counterfactual_list,
+        #    y_source_list=y_source_list,
+        #    y_target_list=y_target_list,
+        #    x_list=x_list,
+        #    base_path=base_dir,
+        #    **kwargs,
+        #)
 
         if is_train:
             self.model.train()
