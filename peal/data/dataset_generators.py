fixed bug with cyclical overflow in square dataset

peal/data/dataset_generators.py
@@ -817,7 +817,9 @@ class SquareDatasetGenerator:
         os.makedirs(os.path.join(self.data_config.dataset_path, "masks"))
         os.makedirs(os.path.join(self.data_config.dataset_path + "_inverse", "imgs"))
         os.makedirs(os.path.join(self.data_config.dataset_path + "_inverse", "masks"))
-        lines_out = ["Name,ClassA,ClassB,ClassC,ClassD,ColorA,ColorB,PositionX,PositionY"]
+        lines_out = [
+            "Name,ClassA,ClassB,ClassC,ClassD,ColorA,ColorB,PositionX,PositionY"
+        ]
         lines_out_inverse = [
             "Name,ClassA,ClassB,ClassC,ClassD,ColorA,ColorB,PositionX,PositionY"
         ]
@@ -857,22 +859,36 @@ class SquareDatasetGenerator:
 
             sample_name = embed_numberstring(sample_idx, 8) + ".png"
             img = np.ones([64, 64, 3], dtype=np.float32) * color_b
-            noise = (np.random.randn(*img.shape) * 10).astype(dtype=np.uint8)
+            noise = np.random.randn(*img.shape) * 10
             img_base = np.clip(img + noise, 0, 255)
             img = np.copy(img_base)
-            img[position_x : position_x + 8, position_y : position_y + 8] = color_a
+            img[position_x : position_x + 8, position_y : position_y + 8] = np.clip(
+                color_a
+                + noise[position_x : position_x + 8, position_y : position_y + 8],
+                0,
+                255,
+            )
             img = Image.fromarray(img.astype(dtype=np.uint8))
             img.save(os.path.join(self.data_config.dataset_path, "imgs", sample_name))
             img_inverse = np.abs(img_base - 255)
-            img_inverse[position_x : position_x + 8, position_y : position_y + 8] = color_a
+            img_inverse[position_x : position_x + 8, position_y : position_y + 8] = np.clip(
+                color_a
+                - noise[position_x : position_x + 8, position_y : position_y + 8],
+                0,
+                255,
+            )
             img_inverse = Image.fromarray(img_inverse.astype(dtype=np.uint8))
             img_inverse.save(
-                os.path.join(self.data_config.dataset_path + "_inverse", "imgs", sample_name)
+                os.path.join(
+                    self.data_config.dataset_path + "_inverse", "imgs", sample_name
+                )
             )
             mask = np.ones([64, 64, 3], dtype=np.uint8) * color_b
             mask[position_x : position_x + 8, position_y : position_y + 8] = 255
             img_mask = Image.fromarray(mask)
-            img_mask.save(os.path.join(self.data_config.dataset_path, "masks", sample_name))
+            img_mask.save(
+                os.path.join(self.data_config.dataset_path, "masks", sample_name)
+            )
             img_mask.save(
                 os.path.join(
                     self.data_config.dataset_path + "_inverse", "masks", sample_name
@@ -904,10 +920,12 @@ class SquareDatasetGenerator:
             ]
             lines_out_inverse.append(",".join(attributes_inverse))
             if (sample_idx + 1) % 100 == 0:
-                open(os.path.join(self.data_config.dataset_path, "data.csv"), "w").write(
-                    "\n".join(lines_out)
-                )
                 open(
-                    os.path.join(self.data_config.dataset_path + "_inverse", "data.csv"),
+                    os.path.join(self.data_config.dataset_path, "data.csv"), "w"
+                ).write("\n".join(lines_out))
+                open(
+                    os.path.join(
+                        self.data_config.dataset_path + "_inverse", "data.csv"
+                    ),
                     "w",
                 ).write("\n".join(lines_out_inverse))

peal/dependencies/glow/train.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from tqdm import tqdm
 import numpy as np
@@ -150,22 +151,22 @@ def train(args, model, optimizer, model_single):
             if i % 100 == 0:
                 with torch.no_grad():
                     utils.save_image(
-                        model_single.reverse(z_sample).cpu().data,
-                        os.path.join(args.base_path, f"sample/{str(i + 1).zfill(6)}.png"),
+                        model_single.reverse(z_sample).cpu().data / 2 + 0.5,
+                        os.path.join(args.base_path, "outputs", f"{str(i + 1).zfill(6)}.png"),
                         normalize=True,
                         nrow=10,
-                        range=(-0.5, 0.5),
+                        #range=(-0.5, 0.5),
                     )
 
             if i % 10000 == 0:
                 torch.save(
-                    model.state_dict(), os.path.join(args.base_path, f"checkpoint/model_{str(i + 1).zfill(6)}.pt")
+                    model.state_dict(), os.path.join(args.base_path, "checkpoint", f"model_{str(i + 1).zfill(6)}.pt")
                 )
                 torch.save(
                     model.state_dict(), os.path.join(args.base_path, f"final.pt")
                 )
                 torch.save(
-                    optimizer.state_dict(), os.path.join(args.base_path, f"checkpoint/optim_{str(i + 1).zfill(6)}.pt")
+                    optimizer.state_dict(), os.path.join(args.base_path, "checkpoint", f"optim_{str(i + 1).zfill(6)}.pt")
                 )
 
 def training(args=None):
@@ -185,7 +186,8 @@ def training(args=None):
     model = model.to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-
+    Path(os.path.join(args.base_path, "outputs")).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(args.base_path, "checkpoint")).mkdir(parents=True, exist_ok=True)
     train(args, model, optimizer, model_single)
 
 if __name__ == "__main__":