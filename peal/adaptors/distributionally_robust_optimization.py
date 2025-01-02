0f6d2780dc351c67dc49371cff0575047fc0a086
Ryan Gelston <rgelston@campus.tu-berlin.de>
2025-01-02 16:57:10 +0100

    Create DRO adaptor. Not tested

--- peal/adaptors/distributionally_robust_optimization.py
+++ peal/adaptors/distributionally_robust_optimization.py
@@ -2,6 +2,8 @@
 from peal.data.datasets import DataConfig
 from peal.adaptors.interfaces import Adaptor, AdaptorConfig
 from peal.training.trainers import TrainingConfig
+from peal.    import
+
 
 
 class DROConfig(AdaptorConfig):
@@ -26,6 +28,10 @@ class DROConfig(AdaptorConfig):
     """
     test_data: DataConfig = None
     """
+    The config of the trainer used for training the model.
+    """
+    training: TrainingConfig = TrainingConfig()
+    """
     The base directory where the run of GroupDRO is stored.
     """
     base_dir: str = "peal_runs/dro"
@@ -49,6 +55,8 @@ class DROConfig(AdaptorConfig):
             so weiter und so fort
         """
 
+        # TODO: We are using pydantic to create the config file. Be sure to check that it's written in this style
+
         if not data is None:
             if isinstance(data, DataConfig):
                 self.data = data
@@ -65,13 +73,34 @@ class DROConfig(AdaptorConfig):
 
 class DRO(Adaptor):
     """
-    NOTE: The simplest way of doing this is to modify the
+    DRO Adaptor docstring.
     """
+
+    # Instantiate the dataset with return_dict=True
+
+    # Pass an instantiated datasets to the model trainer as a list of datasets
+    # [train, val, test] under the datasource keyword
+
+    def __init__(
+        self,
+        datasource: Union[list, tuple] = None,
+        adaptor_config: Union[
+            dict, str, Path, AdaptorConfig
+        ] = "TODO" # TODO: Replace "TODO"
+    ):
+
+        self.adaptor_config = load_yaml_config(adaptor_config, AdaptorConfig)
+
 
 
 
     def run(self):
         """
-        Runs GroupDRO method on
+        Runs GroupDRO method.
         """
+
+        #
+
+
+
         raise NotImplimentedError
