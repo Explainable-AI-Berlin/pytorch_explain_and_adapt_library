bf82fb6adcf0a0ddcaac853630c7dddfdd02eebb
sidney <sb.sidney.bender@gmail.com>
2025-02-12 18:16:54 +0100

    setting up some new experiments for CFKD

--- reproduction_scripts/reproduce_cfkd_results.sh
+++ reproduction_scripts/reproduce_cfkd_results.sh
@@ -1,7 +1,8 @@
 # This script is meant to be able to reproduce the results of the PDC paper (ARXIV_LINK).
 # The results were reproduced with the following software versions: (GIT_HASH)
 # the batch sizes are optimized for a GPU with 80gb VRAM, but can be decreased for smaller GPUs
-# this script was executed at commit 94c3dc8
+# this script was executed at commit TODO
+
 
 # Reproduce the results on the square dataset
 
@@ -11,12 +12,35 @@ python train_generator.py --config "<PEAL_BASE>/configs/generators/square_ddpm.y
 # train the predictor that shall be analyzed
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/square3_classifier_poisoned100.yaml"
 
-# train the teacher for CFKD
-python train_predictor.py --config "<PEAL_BASE>/configs/predictors/square_classifier.yaml"
-
 #
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x100_pdc_unbiased_cfkd.yaml"
 
+
+# Reproduce results on CelebA copyrighttag dataset
+
+# train the generator
+
+
+# Run CFKD for 90% poisoning
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x090_pdc_cfkd_mask.yaml"
+
+# Run CFKD for 100% poisoning
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_pdc_cfkd_mask.yaml"
+
+
+# Reproduce the results on the square dataset
+
+# train the generator
+python train_generator.py --config "<PEAL_BASE>/configs/generators/square_ddpm.yaml"
+
+
+# Reproduce results on Waterbirds dataset
+
+# train the generator that is used for the counterfactual explainer
+python train_generator.py --config "<PEAL_BASE>/configs/generators/waterbirds_ddpm.yaml"
+
+
+# Reproduce results on follicle dataset
 
 # train the generator that is used for the counterfactual explainer (the dataset will be generated automatically)
 python train_generator.py --config "<PEAL_BASE>/configs/generators/follicle_ddpm.yaml"
