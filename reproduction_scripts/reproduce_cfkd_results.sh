464469082514b34028cf1e65179fa16160ae84f3
Sidney Bender <sidney.bender@fokus.fraunhofer.de>
2025-04-10 18:06:41 +0200

    added most configs needed for CFKD paper

--- reproduction_scripts/reproduce_cfkd_results.sh
+++ reproduction_scripts/reproduce_cfkd_results.sh
@@ -14,6 +14,15 @@ python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_pdc_cfk
 # run DFR
 
 
+# Reproduce SOTA results on CelebA Blond_Hair confounding Male task
+python train_generator.py --config "<PEAL_BASE>/configs/generators/celeba_ddpm.yaml"
+python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_Blond_Hair_classifier_poisoned100.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/celeba_Blond_Hair_confounding_Male_poisoned100_pdc_cluster_cfkd.yaml"
+# run GroupDRO
+# run JTT
+# run DFR
+
+
 # Reproduce SOTA results on camelyon17 dataset
 python train_generator.py --config "<PEAL_BASE>/configs/generators/camelyon17_ddpm.yaml"
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/camelyon17_classifier_poisoned100.yaml"
@@ -27,7 +36,7 @@ python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/camelyon17_pdc_cfkd.ya
 # Reproduce SOTA results on follicle dataset
 python train_generator.py --config "<PEAL_BASE>/configs/generators/follicle_ddpm.yaml"
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/follicle_cut_classifier.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_human_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
@@ -38,37 +47,37 @@ python train_generator.py --config "<PEAL_BASE>/configs/generators/celeba_copyri
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_unpoisoned.yaml"
 # For 90% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned090.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x090_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x090_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
 # For 92% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned092.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x092_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x092_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
 # For 94% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned094.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
 # For 96% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned096.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x096_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x096_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
 # For 98% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned098.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x098_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x098_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
 # For 100% poisoning
 python train_predictor.py --config "<PEAL_BASE>/configs/predictors/celeba_copyrighttag_poisoned100.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_pdc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x100_pdc_cfkd.yaml"
 # run GroupDRO
 # run JTT
 # run DFR
@@ -83,19 +92,18 @@ python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_pdc_mas
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_pdc_cluster_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_pdc_human_cfkd.yaml"
 # For Smiling confounding Copyrighttag
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_false_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_true_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_random_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_mask_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_cluster_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x094_pdc_human_cfkd.yaml"
-# For Smiling confounding Copyrighttag
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_false_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_true_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_random_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_mask_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_cluster_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x094_pdc_human_cfkd.yaml"
+# For the follicles dataset
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_false_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_true_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_random_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_mask_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_cluster_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_human_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/follicles_pdc_mask_cfkd.yaml"
 
 
 # Analysis of influence of sample number
@@ -118,10 +126,10 @@ python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_ace_cfk
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_dime_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/square1000x098_fastdime_cfkd.yaml"
 # For Smiling confounding Copyrighttag
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_pfc_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_ace_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_dime_cfkd.yaml"
-python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba500x100_fastdime_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x100_pfc_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x100_ace_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x100_dime_cfkd.yaml"
+python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/Smiling_confounding_CopyrightTag_celeba1000x100_fastdime_cfkd.yaml"
 # For the Camelyon17 dataset
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/camelyon17_ace_cfkd.yaml"
 python run_cfkd.py --config "<PEAL_BASE>/configs/adaptors/camelyon17_dime_cfkd.yaml"
